dd7ed32696b6acefb7b7d1734bbd2227fb06061e
korolevdev <korolevdev@gmail.com>
2017-02-03 01:23:44 +0300

    added url_energy_list route

--- routes.py
+++ routes.py
@@ -4,3 +4,13 @@ from app import *
 from wrappers import *
 from utils import *
 from api_methods import *
+
+@app.route('/energy/list/', methods=['GET'])
+def url_energy_details():
+    id = request.args.get('id')
+
+    fdata = forum_data(id)
+    if fdata:
+        return response_wrap(fdata)
+    else:
+        return response_not_found("Id %s doesn't exist" % forum)
